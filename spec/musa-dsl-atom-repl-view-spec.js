cd740c64006e9a7d71958e2451ee23df9a189d77
Javier Sánchez <javier.sy@gmail.com>
2018-07-03 13:18:23 +0200

    first release

---
+++ spec/musa-dsl-atom-repl-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MusaDslAtomReplView from '../lib/musa-dsl-atom-repl-view';
+
+describe('MusaDslAtomReplView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
